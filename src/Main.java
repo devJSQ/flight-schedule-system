f07f212a9c97dc35cb9ad074598f70bd575a337a
malekcoders <149315149+malekcoders@users.noreply.github.com>
2024-07-21 10:13:19 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
-
 class Main {
-
     public static void main(String args[]){
-        
+        FlightSchedule flightSchedule = new FlightSchedule();
+
+        flightSchedule.addCity(new City("Riyadh"));
+        flightSchedule.addCity(new City("Toktyo"));
+        flightSchedule.addCity(new City("Boston"));
+        flightSchedule.addCity(new City("Kyoto"));
+        flightSchedule.addCity(new City("Jeddah"));
+
+        flightSchedule.addFlight(0, 1);
+        flightSchedule.addFlight(0, 2);
+        flightSchedule.addFlight(1, 3);
+        flightSchedule.addFlight(1, 4);
+        flightSchedule.addFlight(2, 3);
+        flightSchedule.addFlight(3, 4);
+
+        flightSchedule.printFlights();
     }
 }
