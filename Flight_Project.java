d137de00b67a1df2c2ca8843791d0f19e3fd737f
JSQdev <148073798+devJSQ@users.noreply.github.com>
2025-07-12 11:18:11 +0300

    Create Flight_Project.java

---
+++ Flight_Project.java
@@ -0,0 +1,52 @@
+
+class ciity {
+    String name;
+
+    public ciity(String name) {
+        this.name = name;
+    }
+}
+
+class FlightSchedule {
+    private ArrayList<ciity> cities = new ArrayList<>();
+    private int[][] flights = new int[5][5];
+
+    public void addCity(ciity city) {
+        cities.add(city);
+    }
+
+    public void addFlight(int from, int to) {
+        flights[from][to] = 1;
+    }
+
+    public void printFlights() {
+        for (int from = 0; from < cities.size(); from++) {
+            for (int to = 0; to < cities.size(); to++) {
+                if (flights[from][to] == 1) {
+                    System.out.println(cities.get(from).name + " -> " + cities.get(to).name);
+                }
+            }
+        }
+    }
+}
+///////
+public class Main {
+    public static void main(String[] args) {
+        FlightSchedule flightSchedule = new FlightSchedule();
+
+        flightSchedule.addCity(new City("Riyadh"));
+        flightSchedule.addCity(new City("Tokyo"));
+        flightSchedule.addCity(new City("Boston"));
+        flightSchedule.addCity(new City("Kyoto"));
+        flightSchedule.addCity(new City("Jeddah"));
+
+        flightSchedule.addFlight(0, 1);
+        flightSchedule.addFlight(0, 2);
+        flightSchedule.addFlight(1, 3);
+        flightSchedule.addFlight(1, 4);
+        flightSchedule.addFlight(2, 3);
+        flightSchedule.addFlight(3, 4);
+
+        flightSchedule.printFlights();
+    }
+}
