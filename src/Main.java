bf38ac52be55470fde7618115f28ad12125fa494
JSQdev <148073798+devJSQ@users.noreply.github.com>
2025-07-15 04:56:53 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,31 @@
-class Main {
-    public static void main(String args[]){
+import java.util.ArrayList;
+public class FlightSchedule {
+    private ArrayList<City> cities = new ArrayList<>();
+    private int[][] flights = new int[5][5];
+
+    public void addCity(City city) {
+        cities.add(city);
+    }
+
+    public void addFlight(int from, int to) {
+        flights[from][to] = 1;
+    }
+    public void printFlights() {
+        for (int from = 0; from < cities.size(); from++) {
+            for (int to = 0; to < cities.size(); to++) {
+                if (flights[from][to] == 1) {
+                    System.out.println(cities.get(from).name + " -> " + cities.get(to).name);
+                }
+             } 
+        }
+    }
+}
+public class Main {
+    public static void main(String[] args) {
         FlightSchedule flightSchedule = new FlightSchedule();
 
         flightSchedule.addCity(new City("Riyadh"));
-        flightSchedule.addCity(new City("Toktyo"));
+        flightSchedule.addCity(new City("Tokyo"));
         flightSchedule.addCity(new City("Boston"));
         flightSchedule.addCity(new City("Kyoto"));
         flightSchedule.addCity(new City("Jeddah"));
